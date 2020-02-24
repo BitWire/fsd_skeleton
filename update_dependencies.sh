0b7ed223abde491630fba9b2010574edcdc51b6a
Tom Schmidt <alexrider70@gmail.com>
2020-02-24 21:53:18 +0100

    Updates the dependencies and automates the script for Infinity Kerberos

--- update_dependencies.sh
+++ update_dependencies.sh
@@ -10,8 +10,8 @@ WSTOOL_PACKAGE="python-wstool"
 CHECKINSTALL_PACKAGE="checkinstall"
 
 ROS_PACKAGE=(
-    "ros-melodic-desktop-full"
-    "ros-melodic-ros-base"
+    "ros-desktop-full"
+    "ros-ros-base"
 )
 
 FSD_WORKPACKAGES=(
